acf61fe416cf642498be6ee58fe7a6e8bb9cb7d4
Kate <katia230679@mail.ru>
2022-08-18 16:59:47 +0300

    min search added

---
+++ exmp5/Program.cs
@@ -0,0 +1,14 @@
+﻿int a = 2;
+int b = 5;
+int c = 15;
+int d = 8;
+int e = 1;
+
+int min = a;
+if (a < min) min = a;
+if (b < min) min = b;
+if (c < min) min = c;
+if (d < min) min = d;
+if (e < min) min = e;
+
+Console.WriteLine("min is " + min);
