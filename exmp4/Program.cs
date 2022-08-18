f2fa818f5a98290f090f2def4eabe6f91823d1ba
Kate <katia230679@mail.ru>
2022-08-18 16:44:57 +0300

    if else added

---
+++ exmp4/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Представьтесь, пожалуйста");
+string user = Console.ReadLine();
+if (user.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, это же Маша!");
+}
+else
+{
+    Console.WriteLine("Привет, " + user);
+}
